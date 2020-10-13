f4891a25c9dca316578b8269679071d5e644e96c
LB Belella <lb.belella@live.com>
2020-10-12 22:40:50 -0400

    - Added legend - Changed layout to dot - Updated planet ranks

--- build_map.sh
+++ build_map.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-circo -Tpng f2ce_galaxy_map.dot -o f2ce_galaxy_map.png
+dot -Tpng f2ce_galaxy_map.dot -o f2ce_galaxy_map_dot.png
+circo -Tpng f2ce_galaxy_map.dot -o f2ce_galaxy_map_circo.png
+twopi -Tpng f2ce_galaxy_map.dot -o f2ce_galaxy_map_twopi.png
+neato -Tpng f2ce_galaxy_map.dot -o f2ce_galaxy_map_neato.png
+sfdp -Tpng f2ce_galaxy_map.dot -o f2ce_galaxy_map_sfdp.png
+fdp -Tpng f2ce_galaxy_map.dot -o f2ce_galaxy_map_fdp.png
